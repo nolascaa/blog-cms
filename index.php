9af2fa4aa80e287069d2ae6ab258b329d9fbc971
nolascaa <anolc93@gmail.com>
2022-04-11 19:19:20 -0600

    Part 10 completed

--- index.php
+++ index.php
@@ -26,9 +26,24 @@
     </style>
     
     <main>
-        <li>
-            <a href="post.php">Post 1</a>
-        </li>
+      <?php
+          function getPostTitlesFromDatabase() {
+            // TODO in Module 4
+            // get this data from a database instead of hardcoding it
+            $postTitles = array("Blog Post 1", "Blog Post 2", "Blog Post 3");
+            return $postTitles;
+          }
+      ?>
+        <ul>
+          <?php
+              $postTitles = getPostTitlesFromDatabase();
+
+              foreach($postTitles as $postTitle) {
+                  echo "<li><a href='post.php?title=" . $postTitle . "'>" . $postTitle .
+                  "</a></li>";
+              }
+          ?>
+        </ul>
     </main>
 
     <?php
--- post.php
+++ post.php
@@ -26,6 +26,19 @@
     </style>
     
     <main>
+      <?php
+          function getPostDetailsFromDatabase() {
+            //TODO in Module 4
+            // get this data from a database instead of hardcoding it
+            $postDetails = array('title' => 'Blog Post 1',
+                                'content' => 'My first blog post',
+                                'date' => '04/11/2022',
+                                'author' => 'Aaron Nolasco');
+
+            return $postDetails;
+          }
+      ?>
+
         <style>
             main{
                 margin: 0;
@@ -33,18 +46,17 @@
                 background-color: rgb(230, 189, 189);
               }
         </style>
-        <h1>Post 1</h1>
-        <h3>Author: Aaron Nolasco</h3>
-        <h3>Date Published: 4/11/2022</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur eleifend eros at pharetra. Duis bibendum eros a ligula luctus vulputate. 
-            Morbi vestibulum placerat tincidunt. Vivamus molestie pulvinar turpis in feugiat. Vivamus varius faucibus odio at porta. Fusce non mauris rhoncus libero 
-            pulvinar laoreet. Proin gravida ut magna ac tristique.
 
-            Vestibulum facilisis enim eu justo accumsan mollis. Praesent nunc augue, cursus id neque quis, dictum tincidunt enim. Suspendisse viverra congue augue, 
-            in varius lacus lobortis quis. Aenean consequat ultrices nulla id euismod. Duis ac iaculis ex. Etiam dignissim ut magna a accumsan. Fusce accumsan 
-            tortor dignissim quam varius, eget cursus massa vestibulum. Curabitur ante nisl, sagittis sit amet viverra vel, faucibus a magna. Sed viverra, nibh 
-            efficitur fermentum porta, magna tellus aliquet turpis, ac consectetur nunc odio sit amet quam. Sed ultrices nibh orci, sit amet posuere diam accumsan 
-            nec. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In blandit sodales pretium.</p>
+      <?php
+          // Post details contains all the data to generate the blog from
+            $postDetails - getPostDetailsFromDatabase();
+
+      ?>
+      <h1> <?php echo $postDetails["title"]; ?> <h1>
+      <div> <?php echo $postDetails["author"]; ?> </div>
+      <div> <?php echo $postDetails["date"]; ?> </div>
+      <div> <?php echo $postDetails["content"]; ?> </div>
+
     </main>
 
     <?php
