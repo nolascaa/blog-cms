f7d33c1f5a54422e8a4e5598fd6c61e4146a35c8
nolascaa <anolc93@gmail.com>
2022-10-09 01:15:54 -0600

    Part 9 Week 4 v2

--- index.php
+++ index.php
@@ -2,22 +2,20 @@
     include 'header.php';
     include 'nav.php';
     include_once 'db_connect.php'
-?>
-    
-<?php
+
     function getPostDetailsFromDatabase() {
-    // Get the post title
-    $postTitle = rawurldecode($_GET["title"]);
-
-    // Get the post that matches the postTitle
-    include_once 'db_connect.php';
-    $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
-    $result = mysqli_query($conn, $sql);
-
-    // Get the first row from the result as an associative array
-    $postDetails = mysqli_fetch_assoc($result);
-    return $postDetails;
-}
+      // Get the post title
+      $postTitle = rawurldecode($_GET["title"]);
+  
+      // Get the post that matches the postTitle
+      include_once 'db_connect.php';
+      $sql = "SELECT * FROM posts WHERE title='" . $postTitle . "'";
+      $result = mysqli_query($conn, $sql);
+  
+      // Get the first row from the result as an associative array
+      $postDetails = mysqli_fetch_assoc($result);
+      return $postDetails;
+  }
 ?>
 
     <style>
