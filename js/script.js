e2e8d899852167ba1cd5d26d3dbb924c951177ef
nolascaa <anolc93@gmail.com>
2022-10-18 01:16:48 -0600

    Added comments to script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $("#hidden").hover(function() {
-        $(this).css("color", "black");
+    $("#hidden").hover(function() {         //When I hover over text
+        $(this).css("color", "black");      //Change the text color to black
     },
-    function() {
-        $(this).hide();
+    function() {                            //When I hover away from the text
+        $(this).hide();                     //Hide this text
     });
 });
